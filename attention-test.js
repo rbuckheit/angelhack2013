bbc6e47a6ca6716965aeb2b767cc9154993bbf3a
Cai GoGwilt <gogwilt@gmail.com>
2013-05-04 19:23:36 -0700

    Creates attention tracker

---
+++ attention-test.js
@@ -0,0 +1,143 @@
+
+        $(window).load(function() {
+            "use strict";
+
+            // lets do some fun
+            var video = document.getElementById('webcam');
+            var canvas = document.getElementById('canvas');
+            try {
+                compatibility.getUserMedia({video: true}, function(stream) {
+                    try {
+                        video.src = compatibility.URL.createObjectURL(stream);
+                    } catch (error) {
+                        video.src = stream;
+                    }
+                    setTimeout(function() {
+                            video.play();
+                            demo_app();
+                        
+                            compatibility.requestAnimationFrame(tick);
+                        }, 500);
+                }, function (error) {
+                    $('#canvas').hide();
+                    $('#log').hide();
+                    $('#no_rtc').html('<h4>WebRTC not available.</h4>');
+                    $('#no_rtc').show();
+                });
+            } catch (error) {
+                $('#canvas').hide();
+                $('#log').hide();
+                $('#no_rtc').html('<h4>Something goes wrong...</h4>');
+                $('#no_rtc').show();
+            }
+
+            var gui,options,ctx,canvasWidth,canvasHeight;
+            var img_u8,work_canvas,work_ctx,ii_sum,ii_sqsum,ii_tilted,edg,ii_canny;
+            var classifier = jsfeat.haar.frontalface;
+
+            var faceTracker;
+
+            var max_work_size = 160;
+
+            var smoother = new headtrackr.Smoother(0.35,  20);
+
+            var demo_opt = function(){
+                this.min_scale = 1;
+                this.scale_factor = 1.15;
+                this.use_canny = false;
+                this.edges_density = 0.13;
+                this.equalize_histogram = true;
+            }
+
+            function demo_app() {
+                canvasWidth  = canvas.width;
+                canvasHeight = canvas.height;
+                ctx = canvas.getContext('2d');
+
+                ctx.fillStyle = "rgb(0,255,0)";
+                ctx.strokeStyle = "rgb(0,255,0)";
+
+                var scale = Math.min(max_work_size/video.videoWidth, max_work_size/video.videoHeight);
+                var w = (video.videoWidth*scale)|0;
+                var h = (video.videoHeight*scale)|0;
+
+                img_u8 = new jsfeat.matrix_t(w, h, jsfeat.U8_t | jsfeat.C1_t);
+                edg = new jsfeat.matrix_t(w, h, jsfeat.U8_t | jsfeat.C1_t);
+                work_canvas = document.createElement('canvas');
+                work_canvas.width = w;
+                work_canvas.height = h;
+                work_ctx = work_canvas.getContext('2d');
+                ii_sum = new Int32Array((w+1)*(h+1));
+                ii_sqsum = new Int32Array((w+1)*(h+1));
+                ii_tilted = new Int32Array((w+1)*(h+1));
+                ii_canny = new Int32Array((w+1)*(h+1));
+
+                options = new demo_opt();
+
+                faceTracker = new attentiontrackr.FaceTracker(video);
+            }
+
+            var haveAttention = false;
+
+            function tick() {
+                compatibility.requestAnimationFrame(tick);
+                if (video.readyState === video.HAVE_ENOUGH_DATA) {
+
+                    ctx.drawImage(video, 0, 0, canvasWidth, canvasHeight);
+
+                    work_ctx.drawImage(video, 0, 0, work_canvas.width, work_canvas.height);
+                    var imageData = work_ctx.getImageData(0, 0, work_canvas.width, work_canvas.height);
+
+
+                    faceTracker.process(imageData);
+
+                    var updateHaveAttention = faceTracker.isPayingAttention();
+                    if (haveAttention != updateHaveAttention) {
+                        haveAttention = updateHaveAttention;
+                        console.log("have attention", haveAttention);
+                    }
+
+                    var sc = canvasWidth/img_u8.cols;
+                    var r = faceTracker.faceLocation;
+                    drawRect(ctx, r, sc);
+                    r = faceTracker.rightEyeLocation;
+                    drawRect(ctx, r, sc);
+                    r = faceTracker.leftEyeLocation;
+                    drawRect(ctx, r, sc);
+                }
+            }
+
+            function drawRect(ctx, r, sc) {
+                if (r!= null) {
+                    ctx.strokeRect((r.x*sc)|0,(r.y*sc)|0,(r.width*sc)|0,(r.height*sc)|0);
+                }
+            }
+
+            function draw_faces(ctx, rects, sc, max) {
+                var on = rects.length;
+                var r;
+                if(on && max) {
+                    jsfeat.math.qsort(rects, 0, on-1, function(a,b){return (b.confidence<a.confidence);})
+                }
+                if (rects.length > 0) {
+                    if (!smoother.initialized) {
+                        smoother.init(rects[0]);
+                    }
+                    var r = smoother.smooth(rects[0]);
+                    ctx.strokeRect((r.x*sc)|0,(r.y*sc)|0,(r.width*sc)|0,(r.height*sc)|0);
+                }
+                return r;
+                // var n = max || on;
+                // n = Math.min(n, on);
+                // var r;
+                // for(var i = 0; i < n; ++i) {
+                //     r = rects[i];
+                //     ctx.strokeRect((r.x*sc)|0,(r.y*sc)|0,(r.width*sc)|0,(r.height*sc)|0);
+                // }
+            }
+
+            $(window).unload(function() {
+                video.pause();
+                video.src=null;
+            });
+        });
---
+++ libs/js/gaze-tracking/attention-tracking.js
@@ -0,0 +1,84 @@
+"use strict";
+var attentiontrackr = {};
+
+attentiontrackr.tickMillis = 20;
+attentiontrackr.max_work_size = 160;
+
+attentiontrackr.FaceTracker = function(video) {
+	this.scale = Math.min(attentiontrackr.max_work_size/video.videoWidth, attentiontrackr.max_work_size/video.videoHeight);
+  this.w = (video.videoWidth*this.scale)|0;
+  this.h = (video.videoHeight*this.scale)|0;
+
+  this.img_u8 = new jsfeat.matrix_t(this.w, this.h, jsfeat.U8_t | jsfeat.C1_t);
+  this.edg = new jsfeat.matrix_t(this.w, this.h, jsfeat.U8_t | jsfeat.C1_t);
+
+	this.smoother = new headtrackr.Smoother(0.35, attentiontrackr.tickMillis);
+	this.faceLocation = null;
+  this.lastUpdate = new Date();
+
+	this.options = {
+		"equalize_histogram":true,
+		"use_canny":false,
+		"scale_factor":1.15,
+		"min_scale":1,
+		"edges_density":0.13
+	}
+
+  this.ii_sum = new Int32Array((this.w+1)*(this.h+1));
+  this.ii_sqsum = new Int32Array((this.w+1)*(this.h+1));
+  this.ii_tilted = new Int32Array((this.w+1)*(this.h+1));
+  this.ii_canny = new Int32Array((this.w+1)*(this.h+1));
+
+  this.classifier = jsfeat.haar.frontalface;
+}
+
+attentiontrackr.FaceTracker.prototype.isPayingAttention = function() {
+	return new Date() - this.lastUpdate < 1000;
+}
+
+attentiontrackr.FaceTracker.prototype._haarMatch = function(classifier, options) {
+	// precondition: this.img_u8 contains grayscale version of image data.	
+  // possible options
+  if(options.equalize_histogram) {
+      jsfeat.imgproc.equalize_histogram(this.img_u8, this.img_u8);
+  }
+  //jsfeat.imgproc.gaussian_blur(this.img_u8, this.img_u8, 3);
+
+  jsfeat.imgproc.compute_integral_image(this.img_u8, this.ii_sum, this.ii_sqsum, classifier.tilted ? this.ii_tilted : null);
+
+  if(options.use_canny) {
+      jsfeat.imgproc.canny(this.img_u8, this.edg, 10, 50);
+      jsfeat.imgproc.compute_integral_image(this.edg, this.ii_canny, null, null);
+  }
+
+  jsfeat.haar.edges_density = options.edges_density;
+  var rects = jsfeat.haar.detect_multi_scale(this.ii_sum, this.ii_sqsum, this.ii_tilted, options.use_canny? this.ii_canny : null, this.img_u8.cols, this.img_u8.rows, classifier, options.scale_factor, options.min_scale);
+  rects = jsfeat.haar.group_rectangles(rects, 1);
+
+  return rects;
+}
+
+attentiontrackr.rectContains = function(containingRect, rect) {
+	return containingRect.x < rect.x &&
+		containingRect.y < rect.y &&
+		containingRect.x + containingRect.width > rect.x + rect.width &&
+		containingRect.y + containingRect.height > rect.y + rect.height;
+}
+
+attentiontrackr.FaceTracker.prototype.process = function(imageData) {
+	
+  jsfeat.imgproc.grayscale(imageData.data, this.img_u8.data);
+
+  // Track face
+  var rects = this._haarMatch(jsfeat.haar.frontalface, this.options);
+
+  if (rects.length > 0) {
+  	var on = rects.length;
+  	jsfeat.math.qsort(rects, 0, on-1, function(a,b){return (b.confidence<a.confidence);})
+  	if (!this.smoother.initialized) {
+  		this.smoother.init(rects[0]);
+  	}
+  	this.faceLocation = this.smoother.smooth(rects[0]);
+    this.lastUpdate = new Date();
+  }
+}
